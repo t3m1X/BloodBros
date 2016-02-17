6a7301fcb3a26d41850bb24a4286c086ea7e4f21
Sergi Parra i Ramírez <egrupoparra@gmail.com>
2016-02-17 23:28:56 +0100

    Correcting syntaxis error
    Correcting missing ";" in Marcos' line of code.

--- Hello World/main.c
+++ Hello World/main.c
@@ -5,7 +5,7 @@ int main()
 	printf("hello from Sergi");
 	printf("hello from Oscar");
 	printf("hello from Ivan");
-	printf("hello form Marcos")
+	printf("hello form Marcos");
 	getchar();
 	return 0;
 }
