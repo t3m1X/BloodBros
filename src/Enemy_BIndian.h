49bc8bebd2242c1847e1501641d64161da167010
Megaoski <oscar_2596@hotmail.com>
2016-04-27 00:09:12 +0200

    Blue Indian code
    Added the code for implementing the Blue Indian NPC ingame

---
+++ src/Enemy_BIndian.h
@@ -0,0 +1,28 @@
+#ifndef __ENEMY_BINDIAN__
+#define __ENEMY_BINDIAN__
+
+#include "Enemy.h"
+#include "p2Point.h"
+#include "Path.h"
+
+class BIndian : public Enemy
+{
+
+private:
+
+	Animation walk;
+	Animation shoot;
+	Animation dead;
+	p2Point<int> i_pos;
+	Path BIndian_path;
+	Collider* c_BIndian;
+
+public:
+
+	BIndian(int x, int y);
+
+	void Update();
+
+};
+
+#endif 
