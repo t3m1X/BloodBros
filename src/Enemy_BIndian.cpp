05201c41b3760ef3b92e575080d23bb58e25b5ff
Megaoski <oscar_2596@hotmail.com>
2016-04-27 14:20:52 +0200

    Indian implementation

--- src/Enemy_BIndian.cpp
+++ src/Enemy_BIndian.cpp
@@ -5,15 +5,20 @@
 #include "p2Point.h"
 #include "ModuleParticles.h"
 
+#include "SDL/include/SDL.h"
+#pragma comment( lib, "SDL_mixer/libx86/SDL2_mixer.lib" )
+
+
 BIndian::BIndian(int x, int y) : Enemy(x, y)
 {
 	walk.PushBack({ 270, 441, 47, 90});
 	walk.PushBack({ 333, 438, 66, 93});
-	walk.speed = 0.1f;
+	walk.speed = 0.2f;
 
 	shoot.PushBack({ 413, 435, 48, 96});
 	shoot.PushBack({ 478, 438, 48, 93});
 	shoot.speed = 0.1f;
+	shoot.loop = false;
 
 	dead.PushBack({ 548, 435, 84, 96});
 	dead.PushBack({ 652, 456, 48, 75});
@@ -22,16 +27,37 @@ BIndian::BIndian(int x, int y) : Enemy(x, y)
 	dead.speed = 0.1f;
 	
 
-	i_pos.x = x;
-	i_pos.y = y;
 
+	animation = &walk;
 	collider = App->collision->AddCollider({ 0, 0, 70, 70 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
 
+	
+
 	position.x = SCREEN_WIDTH;
+	i_pos.y = y;
 
 }
 
 void BIndian::Move()
 {
+	current_time = SDL_GetTicks();
+
+	if (current_time >= shoot_end)
+	{
+		position.x -= 2;
+		if (has_shot)
+		{
+			position.y = i_pos.y;
+			animation = &walk;
+		}
+	}
+	if (position.x <= SCREEN_WIDTH / 2 - shoot.frames[1].w / 2 && !has_shot)
+	{
+		shoot_end = current_time + 1000;
+		position.y -= 10;
+		animation = &shoot;
+		App->particles->AddParticle(App->particles->shoot, position.x, position.y);
+		has_shot = true;
+	}
 
 }
--- src/Enemy_BIndian.h
+++ src/Enemy_BIndian.h
@@ -15,6 +15,12 @@ private:
 	Animation dead;
 	p2Point<int> i_pos;
 	Path BIndian_path;
+
+
+	Uint32 current_time = 0;
+	Uint32 shoot_end = 0;
+
+	bool has_shot = false;
 	
 
 public:
