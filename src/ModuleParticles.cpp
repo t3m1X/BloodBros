2eba3590c786b03181b412c2768eaad58720ccb4
Sergi Parra i Ramírez <egrupoparra@gmail.com>
2016-04-27 20:47:31 +0200

    Fixed merge issue

--- src/ModuleParticles.cpp
+++ src/ModuleParticles.cpp
@@ -145,13 +145,11 @@ void ModuleParticles::OnCollision(Collider* c1, Collider* c2)
 		// Always destroy particles that collide
 		if (active[i] != nullptr && active[i]->collider == c1)
 		{
-<<<<<<< HEAD
 			AddParticle(bullet_collision, active[i]->position.x, active[i]->position.y);
-=======
 			//AddParticle(explosion, active[i]->position.x, active[i]->position.y);
 			if (App->player->player_collider == c2)
 				App->player->hit = true;
->>>>>>> origin/master
+
 			delete active[i];
 			active[i] = nullptr;
 			break;
