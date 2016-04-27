bab287f17df46b07e503792126d3c646fa99f759
IVREI0T <vpapiol777@gmail.com>
2016-04-27 12:59:19 +0200

    Pushbacks for plane corrected

--- src/Enemy_Plane.cpp
+++ src/Enemy_Plane.cpp
@@ -13,14 +13,14 @@ Plane::Plane(int x) : Enemy(x, 25)
 		position.x = SCREEN_WIDTH / 4 * 3;
 
 
-	fly.PushBack({ 53, 2962, 32, 16 });
-	fly.PushBack({ 101, 2954, 64, 32 });
-	fly.PushBack({ 181, 2937, 128, 56 });
-	fly.PushBack({ 329, 2926, 192, 80 });
-	fly.PushBack({ 550, 2920, 256, 96 });
-	fly.PushBack({ 822, 2914, 319, 123 });
-	fly.PushBack({ 25, 3046, 379, 152 });
-	fly.PushBack({ 459, 3110, 448, 188 });
+	fly.PushBack({ 34, 1974, 22, 10 });
+	fly.PushBack({ 66, 1968, 43, 22 });
+	fly.PushBack({ 120, 1957, 85, 37 });
+	fly.PushBack({ 219, 1951, 128, 53 });
+	fly.PushBack({ 367, 1947, 170, 64 });
+	fly.PushBack({ 548, 1943, 212, 82 });
+	fly.PushBack({ 14, 2031, 253, 101 });
+	fly.PushBack({ 306, 2073, 298, 126 });
 	fly.speed = 0.045f;
 	fly.loop = false;
 
