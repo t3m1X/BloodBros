08c244e451b556565cf82bd443585e97e65f7a12
IVREI0T <vpapiol777@gmail.com>
2016-05-16 20:34:05 +0200

    Added first code for enemies that appear in V

---
+++ src/Enemy_V_Right1.cpp
@@ -0,0 +1,56 @@
+#include "Application.h"
+#include "Enemy_V_Right1.h"
+#include "Path.h"
+#include "ModuleCollision.h"
+#include "p2Point.h"
+#include "ModuleParticles.h"
+
+#include <stdlib.h> 
+#include <time.h>  
+#include "SDL/include/SDL.h"
+#pragma comment( lib, "SDL_mixer/libx86/SDL2_mixer.lib" )
+
+
+Enemy_V_Right1::Enemy_V_Right1(int x, int y) : Enemy(x, y)
+{
+
+	appear.PushBack({ 1218, 2256, 48, 96 });
+	appear.PushBack({ 1266, 2256, 48, 96 });
+	appear.PushBack({ 1314, 2256, 48, 96 });
+	appear.PushBack({ 1362, 2256, 48, 96 });
+	appear.loop = false;
+	appear.speed = 2.0f;
+
+	walk.PushBack({ 864, 2592, 96, 96 });
+	walk.PushBack({ 960, 2592, 96, 96 });
+	walk.PushBack({ 1056, 2592, 96, 96 });
+	walk.PushBack({ 1152, 2592, 96, 96 });
+	walk.PushBack({ 1248, 2592, 96, 96 });
+	walk.PushBack({ 1344, 2592, 96, 96 });
+	walk.speed = 0.2f;
+
+	dead.PushBack({ 1344, 2496, 96, 96 });
+	dead.PushBack({ 1440, 2496, 96, 96 });
+	dead.PushBack({ 1536, 2496, 96, 96 });
+	dead.PushBack({ 1632, 2496, 96, 96 });
+	dead.PushBack({ 1632, 2496, 96, 96 });
+	dead.PushBack({ 1632, 2496, 96, 96 });
+	dead.speed = 0.1f;
+
+	animation = &appear;
+
+	collider = App->collision->AddCollider({ 0, 0, 96, 96 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
+
+
+	position.x = SCREEN_WIDTH;
+	i_pos.y = position.y;
+
+	srand(time(NULL));
+
+}
+
+void Enemy_V_Right1::Move()
+{
+	if (appear.Finished())
+		animation = &walk;
+}
